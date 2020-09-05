15e8f72cab191d400c9610aaa3db2563891c4ab1
ParijatPanda <pandapaga1996@gmail.com>
2020-09-05 22:18:13 +0530

    Summary

---
+++ My Setup Basics.py
@@ -0,0 +1,156 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a=10
+
+print(a)
+
+print?
+
+print("Parijat Panda")
+
+print("Parijat", "Panda", sep ='_')
+print("Parijat", "Panda", sep ='_')
+
+print("Parijat", "Panda", sep ='_', end="  ")
+print("Parijat", "Panda", sep ='_')
+
+help(print)
+
+help("OPERATORS")
+
+
+!pip install pandas
+!python --version
+
+!pip list
+
+a="Parijat"
+b=10
+c=30.33
+
+type(a)
+type(b)
+type(c)
+
+
+
+input("Your Country")
+
+s=input("Your Country")
+
+type(s)
+
+
+
+#type casting
+number= input()
+print(number)
+type(number)
+
+Num = int(number)
+
+type(Num)
+
+
+
+
+country= "India"
+
+print("I live in", country, "which is a very good country")
+print("I live in {0} which is a very good country", format(country))
+
+countrycode= 91
+
+print("I live in", str(countrycode), "which is a very good country")
+print("I live in {0} which is a very good country code is {1}, format(country,countrycode)")
+
+
+
+a=1.11
+b=int(a)
+c=float(b)
+
+x=2
+y=3
+
+z
+
+print(z)
+print(x)
+print(x+y)
+print(x/y)
+
+
+x=5
+x=x-1
+x
+
+x-=1
+x
+
+x*=2
+x
+
+y=x**2
+y
+
+
+'''
+
+
+
+'''
+
+
+
+
+
+Fname="Parijat"
+Lname="Panda"
+
+name=Fname+Lname
+name
+
+name=Fname+"   "+Lname
+name
+
+Fname="Parijat"
+Lname="Panda"
+RollNo=111
+
+name=Fname+"   "+Lname+" "+ str(RollNo)
+name
+
+
+len(Fname)
+
+len(name)
+
+Lname=Lname.capitalize()
+print(Lname)
+
+Lname=Lname.upper()
+print(Lname)
+
+Lname=Lname.lower()
+print(Lname)
+
+Lname=Lname.rjust(10)
+print(Lname)
+
+Lname=Lname.center(15)
+print(Lname)
+
+name = "Parijat Panda 111"
+
+
+name=name.replace('111', '133')
+name
+
+name=name.replace('panda', 'sahu')
+name
